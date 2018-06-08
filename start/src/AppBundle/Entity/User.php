40614c9602c1847a07e609436f992db370042098
abel verbo <havelinux@gmail.com>
2018-06-08 17:09:46 +0200

    add user class

---
+++ start/src/AppBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface
+{
+
+  public function getUsername() {
+    // TODO: Implement getUsername() method.
+  }
+
+  public function getRoles() {
+    // TODO: Implement getRoles() method.
+  }
+
+  public function getPassword() {
+    // TODO: Implement getPassword() method.
+  }
+
+  public function getSalt() {
+    // TODO: Implement getSalt() method.
+  }
+
+  public function eraseCredentials() {
+    // TODO: Implement eraseCredentials() method.
+  }
+
+}
