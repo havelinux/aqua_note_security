b0d0e292bc4bd179e5cf1e63e0af7313643122ac
abel verbo <havelinux@gmail.com>
2018-06-13 16:24:44 +0200

    implements authenticator

--- start/src/AppBundle/Security/LoginFormAuthenticator.php
+++ start/src/AppBundle/Security/LoginFormAuthenticator.php
@@ -3,8 +3,10 @@
 namespace AppBundle\Security;
 
 use AppBundle\Form\LoginForm;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
@@ -12,10 +14,14 @@ use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticato
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
   private $formFactory;
+  private $em;
+  private $router;
 
-  public function __construct(FormFactoryInterface $formFactory)
+  public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router)
   {
     $this->formFactory = $formFactory;
+    $this->em = $em;
+    $this->router = $router;
   }
 
   public function getCredentials(Request $request)
@@ -36,14 +42,31 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
   public function getUser($credentials, UserProviderInterface $userProvider)
   {
+    $username = $credentials['_username'];
+
+    return $this->em->getRepository('AppBundle:User')
+      ->findOneBy(['email' => $username]);
   }
 
   public function checkCredentials($credentials, UserInterface $user)
   {
+    $password = $credentials['_password'];
+
+    if ($password == 'pwdgate') {
+      return true;
+    }
+
+    return false;
   }
 
   protected function getLoginUrl()
   {
+    return $this->router->generate('security_login');
+  }
+
+  protected function getDefaultSuccessRedirectUrl()
+  {
+    return $this->router->generate('homepage');
   }
 
 }
